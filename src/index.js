51498527b41b528511d14f2c5ad99ce513cf5130
Dan Mercer <danielmercer1@gmail.com>
2017-12-26 20:29:02 -0700

    Got gdrive-simple and angular controller working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Potato");
+var gds = require('gdrive-simple');
+
+var gdsInit = gds.init({
+    clientId: "262230741042-6pk925ik2nrmpkp7ekb35r149p1om5t9.apps.googleusercontent.com",
+    scopes: [
+        "https://www.googleapis.com/auth/drive.appdata",
+    ],
+});
+
+angular
+.module('myApp', [])
+.controller('myCtrl', myCtrl);
+
+function myCtrl($scope) {
+	$scope.test = "Hello world!";
+	gdsInit.then(() => {
+    gds.listenForSignInChange(isSignedIn => {
+			console.log("isSignedIn:", isSignedIn);
+		});
+		$scope.test = "GDS initialized";
+		$scope.$apply();
+	});
+}
